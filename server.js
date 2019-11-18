aa18ed3a3474ab9d478f638985f4b85f85fd1da3
Jhosehp Rendon <jhosehp.rendon@gmail.com>
2019-11-18 09:55:59 -0500

    HTTP to HTTPS

--- server.js
+++ server.js
@@ -1,24 +1,17 @@
-var sslRedirect = require('heroku-ssl-redirect');
-
+var redirectToHTTPS = require('express-http-to-https').redirectToHTTPS
+ 
 const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(sslRedirect());
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(redirectToHTTPS([/localhost:(\d{4})/], [/\/insecure/], 301));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-var http = express.createServer();
 
-http.get('*', function(req, res) {  
-  res.redirect('https://' + req.headers.host + req.url);
-
-  // Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
-  // res.redirect('https://example.com' + req.url);
-})
 app.listen(port);
