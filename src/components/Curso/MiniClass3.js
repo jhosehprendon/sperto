1a07de2f16caa2d79eab8fe9e4f437249afc9303
Jhosehp Rendon <jhosehp.rendon@gmail.com>
2020-04-14 09:28:32 -0500

    Add paypal button

--- src/components/Curso/MiniClass3.js
+++ src/components/Curso/MiniClass3.js
@@ -18,6 +18,26 @@ class MiniClass3 extends React.Component {
     },10000)
   }
 
+  showPaypalButton = () => {
+    if(!this.state.hideButton) {
+      return (
+        <div style={{marginBottom: '20px'}}>
+          <p style={{fontWeight: 'bold'}}>Curso de Marketing Digital</p>
+          <p>Antes <span style={{textDecorationLine: 'line-through'}}>$700</span> - Ahora $200</p>
+          <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_s-xclick" />
+            <input type="hidden" name="hosted_button_id" value="PQG66NC59ED5L" />
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
+            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+          </form>
+          <p>Paga seguro con Paypal o tarjeta de crédito</p>
+          <p>Si tienes problemas para realizar el pago envíanos un email: jhosehp@spertomedia.com</p>
+        </div>
+      )
+    }
+    return null 
+  }
+
   sendToPaypal() {
     history.push('/')
   }
@@ -28,16 +48,7 @@ class MiniClass3 extends React.Component {
         <h3><span style={{cursor:'pointer'}} onClick={() => history.push('/mini-class-1')}>1. Oportunidades Digitales </span> > <span style={{cursor:'pointer'}} onClick={() => history.push('/mini-class-2')}>2. Por qué Marketing Digital? </span> > 3. Conviértete en un Digital Marketer</h3> 
         <h2 style={{fontSize: '30px'}}>Comienza tu carrera como Digital Marketer</h2>
         <iframe title="video1" type="text/html" frameborder="0" className="video" src="https://www.youtube.com/embed/YiJ5oAIqXJY" allowfullscreen></iframe>
-        <div>
-          <button 
-            type="button" 
-            style={{margin: '10px 0 10px 0', fontWeight: 'bold', padding: '12px' }} 
-            className={[this.state.hideButton ? 'button-hidden' : '', "btn", "btn-success"].join(' ')}
-            onClick={() => this.sendToPaypal()}
-          >
-            Pagar Curso de Marketing Digital
-          </button>
-        </div>
+        {this.showPaypalButton()}
       </div>
     )
   }
