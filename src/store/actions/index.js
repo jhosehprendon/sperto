7499662f8fa51f93805452dd0b01677f10ba6801
Jhosehp Rendon <jhosehp.rendon@gmail.com>
2020-07-06 00:06:50 -0500

    Add proxy

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -61,7 +61,7 @@ export const createContact = (formValues, tagId, sequenceId) => {
       }
 
       try {
-        const response = await active.post('/contacts', data, {
+        const response = await axios.post('/contacts', data, {
           headers: {
             'Content-type': 'application/json',
             'Api-Token': process.env.REACT_APP_API_AC_TOKEN
@@ -78,7 +78,7 @@ export const createContact = (formValues, tagId, sequenceId) => {
           }
         }
         
-        await active.post('/contactTags', tagData, {
+        await axios.post('/contactTags', tagData, {
           headers: {
             'Content-type': 'application/json',
             'Api-Token': process.env.REACT_APP_API_AC_TOKEN
@@ -94,7 +94,7 @@ export const createContact = (formValues, tagId, sequenceId) => {
           }
         }
         
-        await active.post('/contactAutomations', sequenceData, {
+        await axios.post('/contactAutomations', sequenceData, {
           headers: {
             'Content-type': 'application/json',
             'Api-Token': process.env.REACT_APP_API_AC_TOKEN
