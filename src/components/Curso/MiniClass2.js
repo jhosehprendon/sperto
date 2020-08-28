63acc450fe52b4420474658ce8f36cdefd44bb9a
Jhosehp Rendon <jhosehp.rendon@gmail.com>
2020-08-28 07:03:44 -0500

    Add recursos

--- src/components/Curso/MiniClass2.js
+++ src/components/Curso/MiniClass2.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import history from '../../history';
+import Card from '../Card';
+import { Link } from 'react-router-dom';
 
 class MiniClass2 extends React.Component {
   render() {
@@ -9,6 +11,14 @@ class MiniClass2 extends React.Component {
         <h2 style={{fontSize: '30px'}}>Audiencias e Intereses de Facebook</h2>
         <iframe title="video1" type="text/html" frameborder="0" className="video" src="https://www.youtube.com/embed/r94Nrrh7adI" allowfullscreen></iframe>
         <h3>Espera mañana el siguiente video en tu email</h3>
+        <Card>
+          <div style={{ textAlign: 'center'}}>
+            <h5 style={{color: '#295b8d', fontSize: '20px'}}>Recursos</h5>
+              <Link to="/files/Guia_targeting_research.pdf" target="_blank" download>
+                <button onClick={this.descargaGuia} type="button" style={{marginTop: '10px', fontWeight: 'bold', fontSize: '16px', padding: '12px' }} className="btn btn-success">Descargar Guia de Intereses</button>
+              </Link>
+          </div>
+        </Card>
       </div>
     )
   }
